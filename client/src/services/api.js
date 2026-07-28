dae501750399f46ff40cf7f341c69deb927477d7
Dhrub Kumar Thakur <dhrubkumarthakur267@gmail.com>
2026-07-28 18:05:24 +0530

    Fix frontend API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,30 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://finalhealthmatchhubproject.onrender.com",
+// });
+
+// // Automatically attach JWT token
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default api;
+
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://finalhealthmatchhubproject.onrender.com/api",
 });
 
 // Automatically attach JWT token
