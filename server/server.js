1d845b58eb3f9afe10520a12baaa1bac73cd22e7
Dhrub Kumar Thakur <dhrubkumarthakur267@gmail.com>
2026-07-28 21:46:17 +0530

    Remove debug logs from server startup

--- server/server.js
+++ server/server.js
@@ -1,17 +1,32 @@
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-const result = dotenv.config();
+// const result = dotenv.config();
 
-console.log(result);
-console.log("MONGODB_URI =", process.env.MONGODB_URI);
+// console.log(result);
+// console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
+// const app = require("./app");
+// const connectDB = require("./config/db");
+
+// const PORT = process.env.PORT || 5000;
+
+// connectDB();
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server is running on port ${PORT}`);
+// });
+
+require("dotenv").config();
 
 const app = require("./app");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
+// Connect MongoDB
 connectDB();
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
